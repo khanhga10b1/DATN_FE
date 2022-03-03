002590276c3db34b5719562e81d3bf34a88bed54
st-khanhho <khanh.ho@supremetech.vn>
2022-03-04 01:18:44 +0700

    deploy config

--- src/service/service.js
+++ src/service/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://k-booking.herokuapp.com',
     //baseURL: 'https://booking-hotel-manage.herokuapp.com/api',
     // timeout: 5000,
     headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Accept': 'application/json' }
